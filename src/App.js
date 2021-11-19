3963c7b7f1732da2655bff09ab144b7d4b4dff45
FlorAbril <florenciasoto801@gmail.com>
2021-11-19 19:49:18 -0300

    Implementing dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
+  const [token] = useLocalStorage("token");
   return (
-    <div className="App">
-      
-      <Home/>
+    <Router>
+      {!token ? (
+        <>
+        {console.log(token)}
+        <Routes>
+          <Route path="/" exact element={<Login/>} />
+        </Routes>
+        </>
+      ) : (
+      <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </>
+      )}
+    </Router>
+  );
+}
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Page not found</h1>
     </div>
   );
 }
